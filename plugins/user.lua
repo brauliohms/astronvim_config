08e020dc878c58121407d7a78ea81229717a84e3
Braulio Henrique Marques Souto <braulio@disroot.org>
2023-09-10 00:40:47 -0300

    Adicionando novos plugins como o visualizador de Markdown e melhor assistencia para abertura e fechamento de parenteses

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,49 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  {
+    "styled-components/vim-styled-components",
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
+    "roobert/surround-ui.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "kylechui/nvim-surround",
+      "folke/which-key.nvim",
+    },
+    config = function()
+      require("surround-ui").setup {
+        root_key = "S",
+      }
+    end,
+  },
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  {
+    "xiyaowong/transparent.nvim",
+    cmd = {
+      "TransparentEnable",
+      "TransparentDisable",
+      "TransparentToggle",
+    },
+  },
 }
